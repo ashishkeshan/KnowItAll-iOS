06ad9ccdf567edc76a83b6d9b068bc8b3ce37840
Ashish Keshan <keshan@usc.edu>
2017-10-08 20:25:52 -0700

    update of homepage

--- KnowItAll/HomePage.swift
+++ KnowItAll/HomePage.swift
@@ -27,7 +27,7 @@ class HomePage: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
