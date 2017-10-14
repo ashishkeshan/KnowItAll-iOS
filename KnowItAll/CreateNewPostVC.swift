0a24512f5f836a8f21428dd1734ebc93a9c67408
Sam <shuzawa@usc.edu>
2017-10-13 21:44:23 -0700

    close to finished new review

--- KnowItAll/CreateNewPostVC.swift
+++ KnowItAll/CreateNewPostVC.swift
@@ -7,29 +7,118 @@
 //
 
 import UIKit
+import Cosmos
 
 class CreateNewPostVC: UIViewController {
 
+    @IBOutlet weak var segementedControl: UISegmentedControl!
+    @IBOutlet weak var academic: UIButton!
+    @IBOutlet weak var food: UIButton!
+    @IBOutlet weak var entertain: UIButton!
+    @IBOutlet weak var location: UIButton!
+    @IBOutlet weak var typeField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ratings: CosmosView!
+    @IBOutlet weak var comment: UITextView!
+    
+    //fields to send to backend
+    var category:Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //stars set up
+        ratings.settings.updateOnTouch = true
+        ratings.settings.fillMode = .full
+        ratings.settings.starSize = 35
+        ratings.rating = 0
+        
+        //optional comment set up
+        comment.delegate = self
+        comment.text = "Optional Comments"
+        comment.textColor = UIColor.lightGray
     }
 
+
+
+    @IBAction func pressed1(_ sender: UIButton) {
+        category = 1
+        academic.backgroundColor = UIColor.yellow
+        food.backgroundColor = UIColor.white
+        entertain.backgroundColor = UIColor.white
+        location.backgroundColor = UIColor.white
+        print(category)
+    }
+    @IBAction func pressed2(_ sender: UIButton) {
+        category = 2
+        food.backgroundColor = UIColor.yellow
+        entertain.backgroundColor = UIColor.white
+        location.backgroundColor = UIColor.white
+        academic.backgroundColor = UIColor.white
+        print(category)
+    }
+    @IBAction func pressed3(_ sender: UIButton) {
+        category = 3
+        entertain.backgroundColor = UIColor.yellow
+        food.backgroundColor = UIColor.white
+        location.backgroundColor = UIColor.white
+        academic.backgroundColor = UIColor.white
+        print(category)
+    }
+    @IBAction func pressed4(_ sender: UIButton) {
+        category = 4
+        location.backgroundColor = UIColor.yellow
+        food.backgroundColor = UIColor.white
+        entertain.backgroundColor = UIColor.white
+        academic.backgroundColor = UIColor.white
+        print(category)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        switch segementedControl.selectedSegmentIndex {
+        case 0:
+            print(0)
+//            historyView.isHidden = true
+//            popularView.isHidden = false
+            break;
+        case 1:
+            print(1)
+//            historyView.isHidden = false
+//            popularView.isHidden = true
+            break;
+        default:
+            break;
+        }
     }
-    */
+}
 
+extension CreateNewPostVC: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n")
+        {
+            
+            self.view.endEditing(true);
+            return false;
+        }
+        return true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Optional Comments"
+            textView.textColor = UIColor.lightGray
+        }
+    }
 }
